7c454ee2fe903c8a2ed6647fc8b63181e6c4aa5a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-15 20:20:59 +0300

    Initial Commit

---
+++ AnimateCustomShapesWithAnimateableData/AnimateCustomShapesWithAnimateableDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimateCustomShapesWithAnimateableDataApp.swift
+//  AnimateCustomShapesWithAnimateableData
+//
+//  Created by ramil on 15.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimateCustomShapesWithAnimateableDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
